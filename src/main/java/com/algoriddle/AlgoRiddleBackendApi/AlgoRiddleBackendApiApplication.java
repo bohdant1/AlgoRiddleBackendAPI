991e51b7891f8cecc7b026718251f83ba0b35c97
Bohdan Tymofieienko <b.tymofieienko@gmail.com>
2024-03-10 15:58:48 +0100

    init

---
+++ src/main/java/com/algoriddle/AlgoRiddleBackendApi/AlgoRiddleBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.algoriddle.AlgoRiddleBackendApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoRiddleBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoRiddleBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algoriddle/AlgoRiddleBackendApi/AlgoRiddleBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algoriddle.AlgoRiddleBackendApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoRiddleBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
