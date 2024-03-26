8c2f5f73634abb44614baebdccd39e29a9c07387
Bohdan Tymofieienko <b.tymofieienko@gmail.com>
2024-03-26 19:45:59 +0100

    redesigned FirebaseConfig.java to read from properties

--- src/main/java/com/algoriddle/AlgoRiddleBackendApi/Security/FirebaseConfig.java
+++ src/main/java/com/algoriddle/AlgoRiddleBackendApi/Security/FirebaseConfig.java
@@ -3,19 +3,25 @@ package com.algoriddle.AlgoRiddleBackendApi.Security;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.config.json}")
+    private String firebaseConfigJson;
+
     @PostConstruct
     public void initialize() {
         try {
-            GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream("src/main/resources/algoriddle-firebase-adminsdk.json"));
+            ByteArrayInputStream serviceAccount = new ByteArrayInputStream(firebaseConfigJson.getBytes(StandardCharsets.UTF_8));
+            GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(credentials)
                     .build();
