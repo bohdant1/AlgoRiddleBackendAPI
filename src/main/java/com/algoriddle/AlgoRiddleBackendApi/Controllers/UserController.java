eef56fc4e8539daed6b89d25789c1c093a84244e
Bohdan Tymofieienko <b.tymofieienko@gmail.com>
2024-04-14 18:15:18 +0200

    added algoriddle.live to cors

--- src/main/java/com/algoriddle/AlgoRiddleBackendApi/Controllers/UserController.java
+++ src/main/java/com/algoriddle/AlgoRiddleBackendApi/Controllers/UserController.java
@@ -12,7 +12,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:4200", "http://algoriddlewebui.localhost:9080/", "https://ashy-dune-025dc7903.5.azurestaticapps.net/"})
+@CrossOrigin(origins = {"http://localhost:4200",
+                        "http://algoriddlewebui.localhost:9080/",
+                        "https://ashy-dune-025dc7903.5.azurestaticapps.net/",
+                        "https://algoriddle.live"})
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService users;
